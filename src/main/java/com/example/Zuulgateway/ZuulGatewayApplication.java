8be4a324b6f56c6268cfe7c37c481227da5e758b
bunty <buntyraj568@gmail.com>
2021-06-24 16:17:02 -0400

    Initial commit

---
+++ src/main/java/com/example/Zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.Zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
